1ec2f209d960eff4cfaf7610caf96cda81f4b0d8
jchaeyun <jocheyoon@gmail.com>
2026-04-06 17:17:40 +0900

    "WebConfig의 allowedOrigins 목록에 프론트엔드 배포 주소 추가."

--- src/main/java/com/catchstyle/api/config/WebConfig.java
+++ src/main/java/com/catchstyle/api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 서버의 모든 API 엔드포인트에 대해 통신을 허용함
-                .allowedOrigins("http://localhost:3000") // 접근을 허용할 프론트엔드 주소
+                .allowedOrigins("http://localhost:3000","https://catchstyle-mvp.vercel.app") // 접근을 허용할 프론트엔드 주소
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 HTTP 헤더 허용
                 .allowCredentials(true) // 쿠키나 인증 정보(토큰)를 포함한 요청을 허용함
